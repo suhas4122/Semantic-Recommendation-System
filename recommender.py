c82b36827b8dcf63b128b44df25d212a8b7ab648
Suhas Jain <suhasjain@iitkgp.ac.in>
2023-04-08 23:20:59 +0530

    Made changes to recommender

--- recommender.py
+++ recommender.py
@@ -5,28 +5,33 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 sparql = SPARQLWrapper("http://localhost:3030/all_products/query")
 sparql.setReturnFormat(JSON)
 
+sparql_prefix = """
+    prefix exs: <http://example.org/schema#> 
+    prefix exr: <http://example.org/resource#>
+    prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+"""
+
+
 def get_name_from_uri(uri):
-    sparql.setQuery("""
-        prefix exs: <http://example.org/schema#> 
-        prefix exr: <http://example.org/resource#>
-        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        prefix xsd: <http://www.w3.org/2001/XMLSchema#>
-        
+    sparql.setQuery(sparql_prefix +  """
         SELECT ?name 
         WHERE {
         """
-        + uri + 
-        """ rdfs:label ?name .
+                    + uri +
+                    """ rdfs:label ?name .
         }
     """)
     ret = sparql.queryAndConvert()
     name = ret['results']['bindings'][0]['name']['value']
     return name
 
+
 def get_cosine_similarity(embedding1, embedding2):
-    return np.dot(embedding1, embedding2) / (np.linalg.norm(embedding1) * np.linalg.norm(embedding2))    
-    
+    return np.dot(embedding1, embedding2) / (np.linalg.norm(embedding1) * np.linalg.norm(embedding2))
+
+
 def string_matching_fiter(history_uris, all_product_uris):
     model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     history_names = [get_name_from_uri(uri) for uri in history_uris]
@@ -36,25 +41,21 @@ def string_matching_fiter(history_uris, all_product_uris):
         similarity_scores = []
         for history_name in history_names:
             embeddings = model.encode([name, history_name])
-            similarity_scores.append(get_cosine_similarity(embeddings[0], embeddings[1]))
+            similarity_scores.append(
+                get_cosine_similarity(embeddings[0], embeddings[1]))
         if uri not in history_uris and max(similarity_scores) > 0.5:
             result.append(uri)
     return result
 
+
 def common_purchase_filter(history_uris, user_uri):
     result = {}
     for uri in history_uris:
-        sparql.setQuery("""
-            prefix exs: <http://example.org/schema#> 
-            prefix exr: <http://example.org/resource#>
-            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-            prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            prefix xsd: <http://www.w3.org/2001/XMLSchema#>
-            
+        sparql.setQuery(sparql_prefix +  """
             SELECT ?name 
             WHERE {
             """
-                + uri + """ ^exs:bought/exs:bought ?product.
+                        + uri + """ ^exs:bought/exs:bought ?product.
                 FILTER(?product != """ + uri + """ )
                 FILTER NOT EXISTS {?product ^exs:bought """ + user_uri + """ .}
                 ?product rdfs:label ?name.
@@ -68,20 +69,15 @@ def common_purchase_filter(history_uris, user_uri):
                 result[ret_val['name']['value']] = 1
     return result
 
+
 def category_similarity_filter(history_uris, all_product_uris):
     categories = {}
     for uri in all_product_uris:
-        sparql.setQuery("""
-            prefix exs: <http://example.org/schema#> 
-            prefix exr: <http://example.org/resource#>
-            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-            prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            prefix xsd: <http://www.w3.org/2001/XMLSchema#>
-            
+        sparql.setQuery(sparql_prefix +  """
             SELECT ?categoryLabel 
             WHERE {
                 """
-            + uri + """ exs:category/rdf:rest*/rdf:first ?category.
+                        + uri + """ exs:category/rdf:rest*/rdf:first ?category.
                 ?category rdfs:label ?categoryLabel.
             }
         """)
@@ -90,67 +86,51 @@ def category_similarity_filter(history_uris, all_product_uris):
         for ret_val in ret['results']['bindings']:
             categories[uri].append(ret_val['categoryLabel']['value'])
     # print(categories)
-    
+
     results = []
     for uri in all_product_uris:
         similarities = []
         for history_uri in history_uris:
             # if uri == history_uri:
-            jaccard_similarity = len(set(categories[uri]).intersection(set(categories[history_uri]))) / len(set(categories[uri]).union(set(categories[history_uri])))
+            jaccard_similarity = len(set(categories[uri]).intersection(set(
+                categories[history_uri]))) / len(set(categories[uri]).union(set(categories[history_uri])))
             similarities.append(jaccard_similarity)
-                # print(jaccard_similarity)
-        if max(similarities) > 0.5 and max(similarities) != 1:
+            # print(jaccard_similarity)
+        if max(similarities) > 0.5 and max(similarities) <= 0.75:
             results.append(uri)
     print(results)
     return results
-    
+
+
 def initial_filter(user_uri):
     history_uris = []
-    sparql.setQuery("""
-        prefix exs: <http://example.org/schema#> 
-        prefix exr: <http://example.org/resource#>
-        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        prefix xsd: <http://www.w3.org/2001/XMLSchema#>
-
+    sparql.setQuery(sparql_prefix +  """
         SELECT ?uri
         WHERE {
         """
-        + user_uri + """ exs:bought ?uri. 
+                    + user_uri + """ exs:bought ?uri. 
         }
     """)
     for ret_val in sparql.queryAndConvert()['results']['bindings']:
-        history_uris.append(ret_val['uri']['value'])
-    
+        history_uris.append("exr:" + ret_val['uri']['value'].split('#')[1])
+
     all_products_uris = []
-    sparql.setQuery("""
-        prefix exs: <http://example.org/schema#> 
-        prefix exr: <http://example.org/resource#>
-        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        prefix xsd: <http://www.w3.org/2001/XMLSchema#>
-    
+    sparql.setQuery(sparql_prefix +  """
         SELECT ?uri
         WHERE {
-            ?uri rdf:type exs:Product. 
+            ?uri rdf:type exr:Product. 
         }
     """)
     for ret_val in sparql.queryAndConvert()['results']['bindings']:
-        all_products_uris.append(ret_val['uri']['value'])
-    
-    print(len(history_uris), len(all_products_uris))
-    
-    file = open("data/uris.txt", "r")
-    uris_check = file.read().splitlines()
-    for uri in uris_check:
-        if uri not in all_products_uris:
-            print(uri)
-    
-    # result_1 = string_matching_fiter(history_uris, all_products_uris)
-    # result_2 = category_similarity_filter(history_uris, all_products_uris)
-    # result_3 = common_purchase_filter(history_uris, "exr:user_1")
-    # return 
+        all_products_uris.append(
+            "exr:" + ret_val['uri']['value'].split('#')[1])
 
-# result = 
-initial_filter("exr:user_1")
+    result_1 = string_matching_fiter(history_uris, all_products_uris)
+    result_2 = category_similarity_filter(history_uris, all_products_uris)
+    result_3 = common_purchase_filter(history_uris, user_uri)
+    return result_1
+
+
+result = initial_filter("exr:user_1")
+print(result)
 # print(result.values(), len(result.values()))
