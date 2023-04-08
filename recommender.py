35719694033b40e82180ff183fa2ffa514e05ffc
ashutoshaks <ashu11.03.2002@gmail.com>
2023-04-08 22:46:32 +0530

    Many things

--- recommender.py
+++ recommender.py
@@ -40,20 +40,117 @@ def string_matching_fiter(history_uris, all_product_uris):
         if uri not in history_uris and max(similarity_scores) > 0.5:
             result.append(uri)
     return result
-                
 
-def initial_filter(history_file_name, all_product_file_name):
-    history_file = open(history_file_name, 'r')
+def common_purchase_filter(history_uris, user_uri):
+    result = {}
+    for uri in history_uris:
+        sparql.setQuery("""
+            prefix exs: <http://example.org/schema#> 
+            prefix exr: <http://example.org/resource#>
+            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+            
+            SELECT ?name 
+            WHERE {
+            """
+                + uri + """ ^exs:bought/exs:bought ?product.
+                FILTER(?product != """ + uri + """ )
+                FILTER NOT EXISTS {?product ^exs:bought """ + user_uri + """ .}
+                ?product rdfs:label ?name.
+            }
+        """)
+        ret = sparql.queryAndConvert()
+        for ret_val in ret['results']['bindings']:
+            if ret_val['name']['value'] in result:
+                result[ret_val['name']['value']] += 1
+            else:
+                result[ret_val['name']['value']] = 1
+    return result
+
+def category_similarity_filter(history_uris, all_product_uris):
+    categories = {}
+    for uri in all_product_uris:
+        sparql.setQuery("""
+            prefix exs: <http://example.org/schema#> 
+            prefix exr: <http://example.org/resource#>
+            prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+            
+            SELECT ?categoryLabel 
+            WHERE {
+                """
+            + uri + """ exs:category/rdf:rest*/rdf:first ?category.
+                ?category rdfs:label ?categoryLabel.
+            }
+        """)
+        ret = sparql.queryAndConvert()
+        categories[uri] = []
+        for ret_val in ret['results']['bindings']:
+            categories[uri].append(ret_val['categoryLabel']['value'])
+    # print(categories)
+    
+    results = []
+    for uri in all_product_uris:
+        similarities = []
+        for history_uri in history_uris:
+            # if uri == history_uri:
+            jaccard_similarity = len(set(categories[uri]).intersection(set(categories[history_uri]))) / len(set(categories[uri]).union(set(categories[history_uri])))
+            similarities.append(jaccard_similarity)
+                # print(jaccard_similarity)
+        if max(similarities) > 0.5 and max(similarities) != 1:
+            results.append(uri)
+    print(results)
+    return results
+    
+def initial_filter(user_uri):
     history_uris = []
-    for line in history_file:
-        history_uris.append(line.strip())
-    all_products_file = open(all_product_file_name, 'r')
-    all_products_uris = []
-    for line in all_products_file:
-        all_products_uris.append(line.strip())  
-    result_1 = string_matching_fiter(history_uris, all_products_uris)
-    return result_1
+    sparql.setQuery("""
+        prefix exs: <http://example.org/schema#> 
+        prefix exr: <http://example.org/resource#>
+        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        prefix xsd: <http://www.w3.org/2001/XMLSchema#>
 
-result = initial_filter('user_history.txt', 'uris.txt')
-for uri in result:
-    print(get_name_from_uri(uri))
+        SELECT ?uri
+        WHERE {
+        """
+        + user_uri + """ exs:bought ?uri. 
+        }
+    """)
+    for ret_val in sparql.queryAndConvert()['results']['bindings']:
+        history_uris.append(ret_val['uri']['value'])
+    
+    all_products_uris = []
+    sparql.setQuery("""
+        prefix exs: <http://example.org/schema#> 
+        prefix exr: <http://example.org/resource#>
+        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        prefix xsd: <http://www.w3.org/2001/XMLSchema#>
+    
+        SELECT ?uri
+        WHERE {
+            ?uri rdf:type exs:Product. 
+        }
+    """)
+    for ret_val in sparql.queryAndConvert()['results']['bindings']:
+        all_products_uris.append(ret_val['uri']['value'])
+    
+    print(len(history_uris), len(all_products_uris))
+    
+    file = open("data/uris.txt", "r")
+    uris_check = file.read().splitlines()
+    for uri in uris_check:
+        if uri not in all_products_uris:
+            print(uri)
+    
+    # result_1 = string_matching_fiter(history_uris, all_products_uris)
+    # result_2 = category_similarity_filter(history_uris, all_products_uris)
+    # result_3 = common_purchase_filter(history_uris, "exr:user_1")
+    # return 
+
+# result = 
+initial_filter("exr:user_1")
+# print(result.values(), len(result.values()))
--- turtle.py
+++ turtle.py
@@ -11,10 +11,11 @@ def convert_to_turtle_product(object, turtle_file):
     label = re.sub('[^0-9a-zA-Z ]+', ' ', label)
     label = re.sub(' +', ' ', label)
     label = label.replace(' ', '_')
-    # print(label)
 
     turtle_file.write('exr:' + label + '\n')
     turtle_file.write('\t\trdfs:label ' + '"' + object['Title'] + '"' + ' ;\n')
+    # rdf:type exs:Product
+    turtle_file.write('\t\trdf:type exr:Product ;\n')
     # add price as ^^xsd:float
     turtle_file.write('\t\texs:price ' + '"')
     turtle_file.write(str(object['Price']))
@@ -34,10 +35,22 @@ def convert_to_turtle_product(object, turtle_file):
     manufacturer = manufacturer.replace(' ', '_')
     # if manufacturer is not empty
     if manufacturer != '':
-        turtle_file.write('\t\texs:manufacturer exr:'+ manufacturer + ' .\n\n')
+        turtle_file.write('\t\texs:manufacturer exr:'+ manufacturer + ' ;\n')
     else:
         # insert rdf:nil
-        turtle_file.write('\t\texs:manufacturer rdf:nil .\n\n')
+        turtle_file.write('\t\texs:manufacturer rdf:nil ;\n')
+    # exs:category 
+    turtle_file.write('\t\texs:category ( ')
+    # iterate over all categories
+    for category in object['Category']:
+        # replace all non-alphanumeric characters with empty string
+        category = re.sub('[^0-9a-zA-Z ]+', ' ', category)
+        # replace multiple spaces with single space
+        category = re.sub(' +', ' ', category)
+        # add category as exs:category
+        turtle_file.write('exr:' + category.replace(' ', '_') + ' ')
+    turtle_file.write(') .\n\n')
+    
 
 file = open(file='data/all_products.json', mode='r')
 
@@ -61,16 +74,32 @@ turtle_file.write('@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n\n')
 # create an empty manufacturer set
 manufacturer_set = set()
 
+category_set = set()
+
 for object in data:
     # insert manufacturer in manufacturer_set
     if object['Manufacturer'] != '':
         manufacturer_set.add(object['Manufacturer'])
     convert_to_turtle_product(object, turtle_file)
+    for category in object['Category']:
+        category_set.add(category)
 
 for manufacturer in manufacturer_set:
+    manufacturer_name = manufacturer
     # replace all non-alphanumeric characters with empty string
     manufacturer = re.sub('[^0-9a-zA-Z ]+', ' ', manufacturer)
     # replace multiple spaces with single space
     manufacturer = re.sub(' +', ' ', manufacturer)
-    turtle_file.write('exr:' + manufacturer.replace(' ', '_') + ' ')
-    turtle_file.write('rdfs:label ' + '"' + manufacturer + '"' + ' .\n\n')
+    turtle_file.write('exr:' + manufacturer.replace(' ', '_') + '\n')
+    turtle_file.write('\t\trdf:type exr:Manufacturer ;\n')
+    turtle_file.write('\t\trdfs:label ' + '"' + manufacturer_name + '"' + ' .\n\n')
+
+for category in category_set:
+    category_name = category
+    # replace all non-alphanumeric characters with empty string
+    category = re.sub('[^0-9a-zA-Z ]+', ' ', category)
+    # replace multiple spaces with single space
+    category = re.sub(' +', ' ', category)
+    turtle_file.write('exr:' + category.replace(' ', '_') + '\n')
+    turtle_file.write('\t\trdf:type exr:Category ;\n')
+    turtle_file.write('\t\trdfs:label ' + '"' + category_name + '"' + ' .\n\n')
