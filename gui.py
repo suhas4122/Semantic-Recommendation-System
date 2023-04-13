d17f3eeadd9229c123c1f7a539deae5920ae5064
ashutoshaks <ashu11.03.2002@gmail.com>
2023-04-13 17:44:30 +0530

    added gui and csv files

--- gui.py
+++ gui.py
@@ -1,7 +1,9 @@
 import streamlit as st
-from helper import *
+import recommender
 import json
-        
+import pandas as pd
+import webbrowser
+
 st.set_page_config(
     page_title="Semantic Recommender System",
     page_icon="🎈",
@@ -11,78 +13,67 @@ st.set_page_config(
 st.title("Semantic Recommender System")
 cc1, pad1, cc2, pad2, cc3 = st.columns([10, 2, 10, 2, 10])
 
-def get_uri_data(uri):
-    data = {}
-    sparql.setQuery(sparql_prefix + """
-        SELECT ?name ?price ?image ?rating
-        WHERE {
-            """ + uri + """ rdfs:label ?name;
-                            exs:price ?price;
-                            exs:image ?image;
-                            exs:rating ?rating.
-        }
-    """)
-    data["name"] = sparql.queryAndConvert()['results']['bindings'][0]['name']['value']
-    data["price"] = sparql.queryAndConvert()['results']['bindings'][0]['price']['value']
-    data["image"] = sparql.queryAndConvert()['results']['bindings'][0]['image']['value']
-    data["rating"] = sparql.queryAndConvert()['results']['bindings'][0]['rating']['value']
-    return data
-
 with cc1:
     st.header("User List")
     option = st.selectbox(
         "Select the user",
-        ("User 1", "User 2", "User 3", "User 4", "User 5", "User 6", "User 7", "User 8", "User 9", "User 10")
+        ("User 1", "User 2", "User 3", "User 4", "User 5", "User 6", "User 7", "User 8")
     )
     st.session_state.user_ind = int(option[-1])
-    history_uris = get_history_uris("exr:user_" + str(st.session_state.user_ind))
-    print(history_uris)
-    for uri in history_uris:
-        data = get_uri_data(uri)
+    recommender.user_ind = st.session_state.user_ind
+    recommender.user_uri = "exr:user_" + str(st.session_state.user_ind)
+    recommender.history_uris = recommender.get_history_uris()
+    recommender.hist_categories = recommender.get_hist_categories()
+   
+    for uri in recommender.history_uris:
+        data = recommender.get_uri_data(uri)
         title_container = st.container()
-        col1, gap, col2 = st.columns([6, 1, 20])
+        col1, gap, col2 = st.columns([8, 1, 20])
         with title_container:
             with col1:
-                st.image(data["image"], width = 80)
+                st.image(data["image"], width = 150)
             with col2:
                 st.markdown(f"**{data['name']}**")
-                st.markdown(f"***Price: {data['price']}***")
+                st.markdown(f"***Price: ₹{data['price']}***")
         st.divider()
     
 with cc2:
     st.header("Recommended Products")
-    f = open('data/all_products.json')
-    data = json.load(f)
+    df_res = pd.read_csv("results/recs_" + str(st.session_state.user_ind) + ".csv")
+    uris = df_res["uris"].tolist()
     for i in range(10):
-        prod_data = data[i]
-        stars = int(prod_data["Rating"])
+        uri = uris[i]
+        data = recommender.get_uri_data(uri)
         title_container = st.container()
-        col1, gap, col2 = st.columns([6, 1, 20])
+        col1, gap, col2 = st.columns([8, 1, 20])
         with title_container:
             with col1:
-                st.image(prod_data["Image"], width = 80)
+                st.image(data["image"], width = 150)
             with col2:
-                st.markdown(f"**{prod_data['Title']}**")
-                st.markdown(f"***Price: {prod_data['Price']}***")
-                st.markdown(f"***Rating: {prod_data['Rating']}***")
-                
+                st.markdown(f"**{data['name']}**")
+                st.markdown(f"***Price: ₹{data['price']}***")
+                st.markdown(f"***Rating: {data['rating']}***")
+                if st.button("Buy Now", key = data['link']):
+                    webbrowser.open_new_tab(data["link"])
         st.divider()
     
 with cc3:
     st.header("Other Users Also Bought")
-    items = common_purchase_filter("exr:user_" + str(st.session_state.user_ind))
+    items = recommender.common_purchase_filter("exr:user_" + str(st.session_state.user_ind))
     for (uri, count) in items:
         if count < 2:
             break
         uri = "exr:" + uri.split('#')[1]
-        data = get_uri_data(uri)
+        data = recommender.get_uri_data(uri)
         title_container = st.container()
-        col1, gap, col2 = st.columns([6, 1, 20])
+        col1, gap, col2 = st.columns([8, 1, 20])
         with title_container:
             with col1:
-                st.image(data["image"], width = 80)
+                st.image(data["image"], width = 150)
             with col2:
                 st.markdown(f"**{data['name']}**")
-                st.markdown(f"***Price: {data['price']}***")
-                st.markdown(f"***Price: {data['rating']}***")
+                st.markdown(f"***Price: ₹{data['price']}***")
+                st.markdown(f"***Rating: {data['rating']}***")
+                if st.button("Buy Now", key = data['link']):
+                    webbrowser.open_new_tab(data["link"])
         st.divider()
--- recommender.py
+++ recommender.py
@@ -16,9 +16,7 @@ sparql_prefix = """
     prefix xsd: <http://www.w3.org/2001/XMLSchema#>
 """
 
-
 def get_name_from_uri(uri):
-    # print(uri)
     sparql.setQuery(sparql_prefix + """
         SELECT ?name
         WHERE {
@@ -31,6 +29,24 @@ def get_name_from_uri(uri):
     name = ret['results']['bindings'][0]['name']['value']
     return name
 
+def get_uri_data(uri):
+    data = {}
+    sparql.setQuery(sparql_prefix + """
+        SELECT ?name ?price ?image ?link ?rating
+        WHERE {
+            """ + uri + """ rdfs:label ?name;
+                            exs:price ?price;
+                            exs:image ?image;
+                            exs:link ?link;
+                            exs:rating ?rating.
+        }
+    """)
+    data["name"] = sparql.queryAndConvert()['results']['bindings'][0]['name']['value']
+    data["price"] = sparql.queryAndConvert()['results']['bindings'][0]['price']['value']
+    data["image"] = sparql.queryAndConvert()['results']['bindings'][0]['image']['value']
+    data["link"] = sparql.queryAndConvert()['results']['bindings'][0]['link']['value']
+    data["rating"] = sparql.queryAndConvert()['results']['bindings'][0]['rating']['value']
+    return data
 
 def get_cosine_similarity(embedding1, embedding2):
     return np.dot(embedding1, embedding2) / (np.linalg.norm(embedding1) * np.linalg.norm(embedding2))
@@ -46,9 +62,6 @@ def get_sim_score(uri, history_names):
     similarity_scores.sort(reverse=True)
     top_len = int(len(similarity_scores) * 0.3)
     return sum(similarity_scores[:top_len]) / top_len
-
-# @lru_cache(maxsize=1000)
-
 
 def string_matching_filter(all_product_uris, history_uris):
     history_names = [get_name_from_uri(uri) for uri in history_uris]
@@ -94,11 +107,9 @@ def cat_sim_score(uri, history_uris):
         cat.append(ret_val['categoryLabel']['value'])
     similarities = []
     for history_uri in history_uris:
-        # if uri == history_uri:
         jaccard_similarity = len(set(cat).intersection(set(
             hist_categories[history_uri]))) / len(set(cat).union(set(hist_categories[history_uri])))
         similarities.append(jaccard_similarity)
-        # print(jaccard_similarity)
     max_sim = max(similarities)
     return max_sim
 
@@ -150,21 +161,17 @@ def category_similarity_filter(all_product_uris, history_uris):
         categories[uri] = []
         for ret_val in ret['results']['bindings']:
             categories[uri].append(ret_val['categoryLabel']['value'])
-    # print(categories)
 
     results = []
     for uri in all_product_uris:
         similarities = []
         for history_uri in history_uris:
-            # if uri == history_uri:
             jaccard_similarity = len(set(categories[uri]).intersection(set(
                 categories[history_uri]))) / len(set(categories[uri]).union(set(categories[history_uri])))
             similarities.append(jaccard_similarity)
-            # print(jaccard_similarity)
         max_sim = max(similarities)
         if max_sim > 0.5 and max_sim <= 0.75:
             results.append((uri, max_sim))
-    # print(results)
     return results
 
 
@@ -180,20 +187,15 @@ def initial_filter(user_uri, history_uris):
         all_products_uris.append(
             "exr:" + ret_val['uri']['value'].split('#')[1])
 
-    # print(all_products_uris, history_uris)
     result_1 = string_matching_filter(all_products_uris, history_uris)
     result_2 = category_similarity_filter(all_products_uris, history_uris)
 
-    # name similarity score, category similarity score, rating, how many products of this manufacturer are in the history
-    # create pd dataframe with named index of uri
     df = pd.DataFrame(
         columns=['name_sim_score', 'cat_sim_score', 'rating', 'manuf_count'])
-    # print(result_1)
     for (uri, sc) in result_1:
         df.loc[uri, 'name_sim_score'] = sc
     for (uri, sc) in result_2:
         df.loc[uri, 'cat_sim_score'] = sc
-    print(df)
     return df
 
 
@@ -209,7 +211,6 @@ def get_rating(uri):
     rating = ret['results']['bindings'][0]['rating']['value']
     return rating
 
-
 def get_manufacturer_count(uri, user):
     sparql.setQuery(sparql_prefix + """
         SELECT DISTINCT(?uri)
@@ -223,20 +224,15 @@ def get_manufacturer_count(uri, user):
     ret = sparql.queryAndConvert()
     return len(ret['results']['bindings'])
 
-
 def complete_df(df):
     for uri in df.index:
         df.loc[uri, 'rating'] = get_rating(uri)
-        # if pd.isnull(df.loc[uri, 'rating']):
-        #     df.loc[uri, 'rating'] = 0
         df.loc[uri, 'manuf_count'] = get_manufacturer_count(uri, user_uri)
-        # check if it is Nan
         if np.isnan(df.loc[uri, 'name_sim_score']):
             df.loc[uri, 'name_sim_score'] = get_sim_score(uri, history_uris)
         if np.isnan(df.loc[uri, 'cat_sim_score']):
             df.loc[uri, 'cat_sim_score'] = cat_sim_score(uri, history_uris)
     return df
-
 
 def prod_sim_score(uri_1, uri_2):
     name_1 = get_name_from_uri(uri_1)
@@ -244,10 +240,7 @@ def prod_sim_score(uri_1, uri_2):
     embeddings = model.encode([name_1, name_2])
     return get_cosine_similarity(embeddings[0], embeddings[1])
 
-
 def recommend(df):
-    # df['rating'] = df['rating'] - 2.5
-    # df.loc[df['rating'] < 0, 'rating'] = 0
     df['rec_score'] = (df['name_sim_score'] * 10 + df['cat_sim_score']
                        * 5 + df['rating'] + df['manuf_count'] * 1) * 2.5
     df = df.sort_values(by=['rec_score'], ascending=False)
@@ -256,7 +249,6 @@ def recommend(df):
     i = 0
     while len(recs) < 10 and i < len(df.index):
         curr_uri = df.index[i]
-        # print(curr_uri)
         max_sim_1 = 0
         for uri in recs:
             max_sim_1 = max(max_sim_1, prod_sim_score(curr_uri, uri))
@@ -268,35 +260,30 @@ def recommend(df):
         i += 1
     return recs
 
-# common_purchases = common_purchase_filter(history_uris, user_uri)
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
+for user_ind in range(1, 9):
+    user_uri = "exr:user_" + str(user_ind)
+    history_uris = get_history_uris()
+    hist_categories = get_hist_categories()
+    print("Started processing for user " + user_uri)
 
-user_ind = 1
-user_uri = "exr:user_" + str(user_ind)
+    if os.path.exists('df/df_' + str(user_ind) + '.csv'):
+        df = pd.read_csv('df/df_' + str(user_ind) + '.csv', index_col=0)
+    else:
+        print("Calculating key metrics...")
+        df = initial_filter(user_uri, history_uris)
+        complete_df(df)
+        df.replace('', 0, inplace=True)
+        df.fillna(0, inplace=True)
+        df = df.astype({'name_sim_score': 'float64', 'cat_sim_score': 'float64',
+                    'rating': 'float64', 'manuf_count': 'int64'})
+        df.to_csv('df/df_' + str(user_ind) + '.csv')
 
-# history_uris = get_history_uris()
-# hist_categories = get_hist_categories()
-# model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+    if os.path.exists('results/recs_' + str(user_ind) + '.csv') is False:
+        print("Getting recommendations...")
+        recs = recommend(df)
+        df_res = pd.DataFrame(columns=['uris'])
+        df_res['uris'] = recs
+        df_res.to_csv('results/recs_' + str(user_ind) + '.csv')
 
-
-# # if df.csv exists, load it, otherwise create it
-# if os.path.exists('df/df_' + str(user_ind) + '.csv'):
-#     df = pd.read_csv('df/df_' + str(user_ind) + '.csv', index_col=0)
-# else:
-#     df = initial_filter(user_uri, history_uris)
-#     complete_df(df)
-#     df.replace('', 0, inplace=True)
-#     df.fillna(0, inplace=True)
-#     df = df.astype({'name_sim_score': 'float64', 'cat_sim_score': 'float64',
-#                    'rating': 'float64', 'manuf_count': 'int64'})
-#     df.to_csv('df/df_' + str(user_ind) + '.csv')
-
-# print(df.describe())
-# recs = recommend(df)
-# # print(recs)
-# for uri in recs:
-#     print(get_name_from_uri(uri))
-res = common_purchase_filter(user_uri)
-print(res)
-res2 = [x for x in res if x[1] > 1]
-print(len(res2))
